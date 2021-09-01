d0884d64d40bd2b85e095f7c9cf040fa5ae67999
Andreas <andreaslorozco@gmail.com>
2021-09-01 17:53:03 -0600

    complete lesson 02

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('hola mundo!!!');
+var htmlBody = document.querySelector('body');
+console.log(htmlBody);
+var channel = 'webtoriales';
+console.log(channel);
+var year = 2021;
---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log('hola mundo!!!');
+
+const htmlBody = document.querySelector('body');
+
+console.log(htmlBody);
+
+const channel = 'webtoriales';
+
+console.log(channel);
+
+let year = 2021;
+
+year = 1999;
