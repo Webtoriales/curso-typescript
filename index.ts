001c46e2577f9f98bebec25ab66476548635f659
Andreas <andreaslorozco@gmail.com>
2021-09-19 12:38:54 -0600

    final code

--- index.ts
+++ index.ts
@@ -4,27 +4,23 @@ let channel = { name: 'webtoriales' };
 
 // channel = { age: 3 };
 // channel = { name: true };
-// channel = { name: 'otro canal'};
+// channel = { name: 'otro canal' };
+// channel.name = '40';
 
-// channel.name = 40;
-
-// let obj: object;
+let obj: object;
 
 // obj = { hola: 'mundo' };
-// obj = { test: true };
-// obj.age = 20;
+// obj = { test: false };
+// obj.age - 20;
+// obj = { test: false, age: 20 };
 // obj = [1, 2, 3];
 
-let superHero: { 
-  name: string,
-  age: number,
-  superpowers: string[] 
-};
+let superHero: { name: string, age: number, superpowers: string[] };
 
 superHero = {
   name: 'wanda',
   age: 30,
-  superpowers: ['usar magia', 'volar']
+  superpowers: ['usar magia', 'volar'],
 };
 
 superHero.name = 'Wanda';
