d9f7fbf7a536f0eac9c2a816e9ab6d7bd5278b7d
Andreas <andreaslorozco@gmail.com>
2021-10-02 19:04:31 -0600

    completes lesson 07

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 type ID = string | number;
+type Year = string | number;
+type Channel = {
+  id: ID,
+  name: string,
+  subscribers: number,
+  year: Year,
+};
 
 const getChannelName = (id: ID) => {
   console.log(`El canal con id ${id} se llama Webtoriales`);
 }
 
-const getChannelInfo = (channel: { id: ID, name: string, subscribers: number, year: ID }) => {
+const getChannelInfo = (channel: Channel) => {
   console.log(`${channel.name} tiene ${channel.subscribers} suscriptores y fue creado en ${channel.year}`);
 }
 
-const webtoriales = {
+const subscribeToChannel = (channel: Channel) => {
+  console.log(`Te has suscrito a ${channel.name}.`);
+}
+
+const webtoriales: Channel = {
   id: 123,
   name: 'webtoriales',
   subscribers: 1800,
@@ -19,6 +30,4 @@ getChannelName('123');
 getChannelName(456);
 getChannelName(webtoriales.id);
 getChannelInfo(webtoriales);
-
-getChannelName(true);
-getChannelInfo({ name: 'hello' });
+subscribeToChannel(webtoriales);
